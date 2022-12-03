d670b84e0376f098e9e3f122ccbe08fdba6e0f45
Rohit Rathi <r-rathi@users.noreply.github.com>
2022-12-02 23:20:53 -0800

    AoC 2022 day 1.

---
+++ 2022/day01/v1.py
@@ -0,0 +1,19 @@
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+
+calories = [0]
+
+for line in lines:
+    line = line.strip()
+    if line:
+        calories[-1] += int(line)
+    else:
+        calories.append(0)
+
+# print(calories)
+# print(len(calories))
+print("part 1:", max(calories))
+
+top_3 = sorted(calories)[-3:]
+# print(top_3)
+print("part 2:", sum(top_3))
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# advent-of-code
-Advent of Code
+# Advent of Code
+
+This repository contains my solutions to the [Advent of Code](https://adventofcode.com)
+programming puzzles.
+
+The solutions are organized by year and day of the puzzle. Some
+solutions may have multiple versions based on how I initially solved the problem and
+later refinements or refactoring.

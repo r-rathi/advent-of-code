7c6f1d9bff0d4f22c5ecbc2dfeac4d683f961a2b
Rohit Rathi <rrathi@earth.lan>
2023-12-12 23:26:25 -0800

    AoC 2023 day 8.

---
+++ 2023/day08/v1.py
@@ -0,0 +1,69 @@
+"""Day 8: Haunted Wasteland"""
+
+import argparse
+import itertools
+import math
+import pathlib
+import re
+import time
+
+
+def part1(network, instructions):
+    inst = itertools.cycle(instructions)
+    node = "AAA"
+    t = 0
+    while node != "ZZZ":
+        # print(t, node)
+        node = network[node][next(inst)]
+        t += 1
+
+    return t
+
+
+def part2(network, instructions):
+    num_insts = len(instructions)
+    start_nodes = [node for node in network if node.endswith("A")]
+    periods = []
+    for start_node in start_nodes:
+        inst = itertools.cycle(instructions)
+
+        states = {}
+        sequence = []
+        node = start_node
+        t = 0
+        while (node, t % num_insts) not in states:
+            # print(t, t % num_insts, node)
+            states[(node, t % num_insts)] = t
+            sequence.append(node[-1])
+            node = network[node][next(inst)]
+            t += 1
+
+        t_repeat = states[(node, t % num_insts)]
+
+        offset = sequence.index("Z")
+        period = len(sequence) - t_repeat
+        # print(offset, period)
+
+        periods.append(period)
+
+    return math.lcm(*periods)
+
+
+# noinspection DuplicatedCode
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", default="input.txt", help="input (default: input.txt)")
+    args = parser.parse_args()
+
+    insts, network_lines = pathlib.Path(args.f).read_text().split("\n\n")
+    network = {}
+    for line in network_lines.splitlines():
+        node, left, right = re.findall(r"[A-Z0-9][A-Z0-9][A-Z]", line)
+        network[node] = dict(L=left, R=right)
+
+    print("part 1:", part1(network, insts))
+    print("part 2:", part2(network, insts))
+
+
+if __name__ == "__main__":
+    main()
